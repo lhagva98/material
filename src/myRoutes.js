55687fb8d3316ba17cdf4e23e3d0c73491f2fe4c
Badrangui A <abadrangui@gmail.com>
2020-04-18 19:31:39 +0800

    first commit

---
+++ src/myRoutes.js
@@ -0,0 +1,60 @@
+import GroupIcon from '@material-ui/icons/Group';
+import AssignmentIcon from '@material-ui/icons/Assignment';
+//import ApartmentIcon from '@material-ui/icons/Apartment';
+import BusinessIcon from '@material-ui/icons/Business';
+import ListAltIcon from '@material-ui/icons/ListAlt';
+import Dashboard from "@material-ui/icons/Dashboard";
+
+import DashboardPage from 'views/DashboardPage';
+import CompanyPage from 'views/CompanyPage';
+import DepartmentPage from 'views/DepartmentPage';
+import EmployeePage from 'views/EmployeePage';
+import PlanPage from 'views/PlanPage';
+import AssignmentPage from 'views/AssignmentPage';
+
+const routes = [
+    {
+        path: "/dashboard",
+        name: "Үзүүлэлт",
+        icon: Dashboard,
+        component: DashboardPage,
+        layout: "/home"
+    },
+    {
+        path: "/company",
+        name: "Байгууллага",
+        icon: BusinessIcon,
+        component: CompanyPage,
+        layout: "/home"
+    },
+    {
+        path: "/department",
+        name: "Хэлтэс",
+        icon: BusinessIcon,
+        component: DepartmentPage,
+        layout: "/home"
+    },
+    {
+        path: "/employee",
+        name: "Ажилчид",
+        icon: GroupIcon,
+        component: EmployeePage,
+        layout: "/home"
+    },
+    {
+        path: "/plan",
+        name: "Төлөвлөгөө",
+        icon: AssignmentIcon,
+        component: PlanPage,
+        layout: "/home"
+    },
+    {
+        path: "/assignment",
+        name: "Ажлууд",
+        icon: ListAltIcon,
+        component: AssignmentPage,
+        layout: "/home"
+    },
+]
+
+export default routes;
