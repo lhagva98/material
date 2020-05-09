1db69232e7b48d8b271bf59af7247bef2c5dbbcc
Badrangui ARIUNBOLD <abadrangui@gmail.com>
2020-05-09 18:18:08 +0800

    shuud shaasan ni

--- src/Axios.js
+++ src/Axios.js
@@ -1,9 +1,8 @@
 import Axios from "axios";
 
-const backendURL = process.env.BACKEND_URL || 'http://localhost:3001/';
 
 export default function AxiosConfig () {
-    Axios.defaults.baseURL =  backendURL;
+    Axios.defaults.baseURL =  "https://epes-tergel-api.herokuapp.com/";
     Axios.defaults.headers.common['Accept'] = 'application/json';
     Axios.defaults.headers.common['Content-Type'] = 'application/json';
 };
