f645cd381a0b9409bd78577fb3fc3c2f5afcb653
Badrangui ARIUNBOLD <abadrangui@gmail.com>
2020-05-09 17:34:08 +0800

    axios backend url env

--- src/Axios.js
+++ src/Axios.js
@@ -1,7 +1,9 @@
 import Axios from "axios";
 
+const backendURL = process.env.BACKEND_URL || 'http://localhost:3001/';
+
 export default function AxiosConfig () {
-    Axios.defaults.baseURL= process.env.BACKEND_URL || `http://localhost:3001/`;
+    Axios.defaults.baseURL =  backendURL;
     Axios.defaults.headers.common['Accept'] = 'application/json';
     Axios.defaults.headers.common['Content-Type'] = 'application/json';
 };
