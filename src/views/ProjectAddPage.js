9cc77a7766417d0abaa18099ecad7be5bd0dc24d
Badrangui <abadrangui@gmail.com>
2021-04-09 07:50:32 +0800

    material added ui done

--- src/views/ProjectAddPage.js
+++ src/views/ProjectAddPage.js
@@ -2,7 +2,18 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
-import { Modal, TextField, MenuItem, Chip } from '@material-ui/core';
+import {
+  Modal,
+  TextField,
+  Button as MButton,
+  Chip,
+  Table,
+  TableHead,
+  TableBody,
+  TableRow,
+  TableCell,
+  Checkbox,
+} from '@material-ui/core';
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -15,21 +26,67 @@ import AddProjectCard from "components/AddProjectCard";
 import { firestore } from 'firebase';
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import Select from 'react-select';
+import DeleteIcon from '@material-ui/icons/DeleteForever';
 
 import Loading from './Loading';
-import Table from "./EPES-components/EPEStable.js";
 import 'react-big-calendar/lib/css/react-big-calendar.css';
+import materiallist from "materialList.json";
 
 
 const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const materialData = [
+  {
+    id: 1,
+    name: 'Банз',
+    unit: 'ш',
+    quantity: 4,
+    prePrice: 20000,
+    price: 18000,
+    total: 72000,
+    done: true,
+  },
+  {
+    id: 2,
+    name: 'Цемент',
+    unit: 'кг',
+    quantity: 10,
+    prePrice: 5000,
+    price: 5000,
+    total: 50000,
+    done: false,
+  },
+  {
+    id: 3,
+    name: 'Плита',
+    unit: 'м2',
+    quantity: 50,
+    prePrice: 3500,
+    price: 0,
+    total: 150000,
+    done: false,
+  },
+]
+
+const activityData = [
+  {
+    id: 1,
+    name: 'Шал',
+  },
+  {
+    id: 2,
+    name: 'Хана',
+  }
+]
 
 function rand() {
   return Math.round(Math.random() * 20) - 10;
 }
 
 function getModalStyle() {
-  const top = 50 + rand();
-  const left = 50 + rand();
+  const top = 50;
+  const left = 50;
 
   return {
     top: `${top}%`,
@@ -53,6 +110,18 @@ const styles = {
       fontWeight: "400",
       lineHeight: "1"
     }
+  },
+  paper: {
+    position: 'absolute',
+    // width: 400,
+    backgroundColor: 'white',
+    boxShadow: 5,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'stretch',
   },
   mainCard: {
     backgroundColor: "transparent",
@@ -88,6 +157,28 @@ const styles = {
   xCell: {
     marginRight: 8,
   },
+  paperLeft: {
+    width: 300,
+  },
+  paperRight: {
+    width: 600,
+    marginLeft: 20,
+    height: '100%',
+  },
+  addMaterialButton: {
+    width: '100%',
+    alignSelf: 'flex-end',
+  },
+  mInput: {
+    width: '100%',
+  },
+  materialRightButtonBox: {
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  selectInputStyle: {
+    zIndex: 20,
+  }
 };
 
 const useStyles = makeStyles(styles);
@@ -110,16 +201,9 @@ export default function DepartmentPage() {
   const [events, setEvents] = useState([]);
   const [popup, setPopup] = useState(true);
   const [pcoordinate, setPcoordinate] = useState({ x: 600, y: 300 })
-  const [activities, setActivities] = useState([
-    {
-      id: 1,
-      name: 'Шал',
-    },
-    {
-      id: 2,
-      name: 'Хана',
-    }
-  ])
+  const [activities, setActivities] = useState(activityData);
+  const [editActivity, setEditActivity] = useState(true);
+  const [materials, setMaterials] = useState(materialData);
 
   useEffect(() => {
 
@@ -147,6 +231,114 @@ export default function DepartmentPage() {
 
   return (
     <GridContainer>
+      <Modal
+        open={true}
+        onClose={() => setEditModalOpen(false)}
+        aria-labelledby="simple-modal-title"
+        aria-describedby="simple-modal-description"
+      >
+        <div style={modalStyle} className={classes.paper}>
+          {/* left side */}
+          <div className={classes.paperLeft} >
+            <h5>Хана</h5>
+            <span>Материал нэмэх</span>
+            <Select
+              isSearchable={true}
+              options={materiallist}
+              className={classes.selectInputStyle}
+            />
+            <TextField className={classes.mInput} value={name} margin="dense" label="Материалын нэр" variant="outlined" />
+            <TextField className={classes.mInput} value={desc} margin="dense" label="Тоо хэмжээ" variant="outlined" />
+            <TextField className={classes.mInput} value={desc} margin="dense" label="Санал болгох үнэ" variant="outlined" style={{ marginBottom: 40 }} />
+
+            <MButton
+              size="large"
+              color="primary"
+              variant="contained"
+              className={classes.addMaterialButton}
+            >
+              Нэмэх
+            </MButton>
+          </div>
+          {/* right side */}
+          <div className={classes.paperRight}>
+            <div style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+              <Button
+                // onClick={handleSaveButtonClick}
+                color="success"
+                size="sm"
+                round
+              >
+                Хадгалах
+              </Button>
+            </div>
+            <div>
+              Нийт материалууд
+            </div>
+            <div>
+              <Table size="small" >
+                <TableHead>
+                  <TableRow>
+                    <TableCell>#</TableCell>
+                    <TableCell>Материалын нэр</TableCell>
+                    <TableCell>х/Нэгж</TableCell>
+                    <TableCell>Тоо ширхэг</TableCell>
+                    <TableCell>Санал болгох үнэ</TableCell>
+                    <TableCell>Авсан үнэ</TableCell>
+                    <TableCell>Нийт</TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableHead>
+
+                <TableBody>
+                  {materials.map((item) => (
+                    <TableRow>
+                      <TableCell><DeleteIcon /></TableCell>
+                      <TableCell>{item.name}</TableCell>
+                      <TableCell>{item.unit}</TableCell>
+                      <TableCell>{item.quantity}</TableCell>
+                      <TableCell>{item.prePrice}</TableCell>
+                      <TableCell>{item.price}</TableCell>
+                      <TableCell>{item.total}</TableCell>
+                      <TableCell style={{ padding: 0 }} >
+                        <Checkbox
+                          // checked={state.checkedB}
+                          // onChange={handleChange}
+                          name="checkedB"
+                          color="primary"
+                          style={{ padding: 0 }}
+                        />
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </div>
+            <div>
+              Нийт дүн: {() => { let total = 0; materials.forEach((item) => total += item.total); return <span>{total}</span> }}
+            </div>
+            <div className={classes.materialRightButtonBox}>
+              <MButton
+                size="large"
+                color="secondary"
+                variant="contained"
+                className={classes.asd}
+              >
+                Буцах
+              </MButton>
+              <MButton
+                size="large"
+                color="default"
+                variant="contained"
+                className={classes.asd}
+              >
+                Хэвлэх
+            </MButton>
+            </div>
+          </div>
+        </div>
+      </Modal>
+
       <GridItem xs={12}>
         {popup ? <Card style={{
           marginTop: 0,
@@ -162,23 +354,33 @@ export default function DepartmentPage() {
         }}>
           <div className={classes.popupCircle} />
           <CardHeader>
-            <h6>Ажлын жагсаалт</h6>
-            <div>
-              <TextField margin="dense" id="outlined-basic" label="Үйл ажиллагааны нэр" variant="outlined" size="small" />
-              <Button variant="contained" color="primary" style={{ marginLeft: 12 }} >
-                Нэмэх
-              </Button>
-            </div>
-            <div>
-              {activities.map((item, index) => (
-                <div className={classes.activityRow}>
-                  <div className={classes.indexCell} >{index + 1}</div>
-                  <div className={classes.nameCell} >{item.name}</div>
-                  <Chip label="Материал нэмэх" onClick={() => console.log("fjdklsjf")} className={classes.addButtonCell} size="small" />
-                  <Chip onDelete={() => console.log("deletechip ")} size="small" />
+            {!editActivity ? (
+              <>
+                <h6>Материал нэмэх</h6>
+                <div>
                 </div>
-              ))}
-            </div>
+              </>
+            ) : (
+              <>
+                <h6>Ажлын жагсаалт</h6>
+                <div>
+                  <TextField margin="dense" id="outlined-basic" label="Үйл ажиллагааны нэр" variant="outlined" size="small" />
+                  <Button variant="contained" color="primary" style={{ marginLeft: 12, alignSelf: 'flex-end' }} >
+                    Нэмэх
+                  </Button>
+                </div>
+                <div>
+                  {activities.map((item, index) => (
+                    <div className={classes.activityRow}>
+                      <div className={classes.indexCell} >{index + 1}</div>
+                      <div className={classes.nameCell} >{item.name}</div>
+                      <Chip label="Материал нэмэх" onClick={() => { console.log("fjdklsjf"); setEditModalOpen(true) }} className={classes.addButtonCell} size="small" />
+                      <Chip onDelete={() => console.log("deletechip ")} size="small" />
+                    </div>
+                  ))}
+                </div>
+              </>
+            )}
           </CardHeader>
         </Card> : null}
         <Card className={classes.mainCard}>
@@ -209,6 +411,6 @@ export default function DepartmentPage() {
           </CardBody>
         </Card>
       </GridItem>
-    </GridContainer>
+    </GridContainer >
   );
 }
